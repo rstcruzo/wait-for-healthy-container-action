f274ca38c785e26fb6ac0f39e5a917cfd574cc42
Rodrigo Santa Cruz Ortega <rstcruzo@gmail.com>
2020-07-22 15:36:18 -0500

    Add base project.

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+
+const exec = require('child_process').exec;
+const sleep = require('sleep');
+const performance = require('perf_hooks').performance;
+
+
+try {
+  const container = core.getInput('container');
+  const maxRetries = core.getInput('max-retries');
+  const interval = core.getInput('interval');
+
+  const getHealthStatusCmd =
+    `docker inspect --format '{{ .State.Health.Status }}' $(docker-compose ps -q ${container})`;
+
+  const t0 = performance.now();
+
+  for (let i = 0; i < maxRetries; i++) {
+    exec(getHealthStatusCmd, function (error, stdOut, stdErr) {
+      if (stdOut == 'healthy') {
+        break;
+      }
+    });
+    sleep.sleep(interval);
+  }
+
+  const t1 = performance.now();
+  core.setOutput("time-spent", t1 - t0);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
